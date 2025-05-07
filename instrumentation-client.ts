aba9a5b2cb1569abd9d6ae276c7244aabb9d2d22
TheDanniCraft <mail@thedannicraft.de>
2025-05-07 10:48:58 +0200

    🔊 Turn off logs on dev env

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
 	dsn: process.env.ERROR_DSN,
+	enabled: process.env.NODE_ENV === "production",
 	integrations: [],
 	tracesSampleRate: 0,
 });
