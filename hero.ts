a992bf065b1922c572fd9ab1047941fb5c710e68
TheDanniCraft <mail@thedannicraft.de>
2025-05-11 20:26:39 +0200

    💄 Add darkmode transition

--- hero.ts
+++ hero.ts
@@ -1,72 +1,72 @@
 import { heroui } from "@heroui/react";
 
 export default heroui({
-        themes: {
-            light: {
-                colors: {
-                    primary: {
-                        DEFAULT: "#5F06F5",
-                        foreground: "#FFFFFF",
-                        50: "#F4E9FF",
-                        100: "#E6CCFE",
-                        200: "#CB9AFE",
-                        300: "#AA68FC",
-                        400: "#8D42F9",
-                        500: "#5F06F5",
-                        600: "#4904D2",
-                        700: "#3603B0",
-                        800: "#26018E",
-                        900: "#1A0175"
-                    },
-                    secondary: {
-                        DEFAULT: "#D23B5D",
-                        50: "#F8D0D6",
-                        100: "#F0A8B3",
-                        200: "#E77D91",
-                        300: "#E05A6F",
-                        400: "#D73A4D",
-                        500: "#D23B5D",
-                        600: "#B72F4F",
-                        700: "#9B2440",
-                        800: "#7F1932",
-                        900: "#631127"
-                    },
-                    background: "#f9fafb",
-                    foreground: "#1c1c1e"
-                }
-            },
-            dark: {
-                colors: {
-                    background: "#242429",
-                    primary: {
-                        DEFAULT: "#5F06F5",
-                        foreground: "#FFFFFF",
-                        50: "#F4E9FF",
-                        100: "#E6CCFE",
-                        200: "#CB9AFE",
-                        300: "#AA68FC",
-                        400: "#8D42F9",
-                        500: "#5F06F5",
-                        600: "#4904D2",
-                        700: "#3603B0",
-                        800: "#26018E",
-                        900: "#1A0175"
-                    },
-                    secondary: {
-                        DEFAULT: "#D23B5D",
-                        50: "#F8D0D6",
-                        100: "#F0A8B3",
-                        200: "#E77D91",
-                        300: "#E05A6F",
-                        400: "#D73A4D",
-                        500: "#D23B5D",
-                        600: "#B72F4F",
-                        700: "#9B2440",
-                        800: "#7F1932",
-                        900: "#631127"
-                    },
-                    foreground: "#f0f0f4"
-                }
-            }
-        }
+	themes: {
+		light: {
+			colors: {
+				primary: {
+					DEFAULT: "#5F06F5",
+					foreground: "#FFFFFF",
+					50: "#F4E9FF",
+					100: "#E6CCFE",
+					200: "#CB9AFE",
+					300: "#AA68FC",
+					400: "#8D42F9",
+					500: "#5F06F5",
+					600: "#4904D2",
+					700: "#3603B0",
+					800: "#26018E",
+					900: "#1A0175",
+				},
+				secondary: {
+					DEFAULT: "#D23B5D",
+					50: "#F8D0D6",
+					100: "#F0A8B3",
+					200: "#E77D91",
+					300: "#E05A6F",
+					400: "#D73A4D",
+					500: "#D23B5D",
+					600: "#B72F4F",
+					700: "#9B2440",
+					800: "#7F1932",
+					900: "#631127",
+				},
+				background: "#f9fafb",
+				foreground: "#1c1c1e",
+			},
+		},
+		dark: {
+			colors: {
+				background: "#242429",
+				primary: {
+					DEFAULT: "#5F06F5",
+					foreground: "#FFFFFF",
+					50: "#F4E9FF",
+					100: "#E6CCFE",
+					200: "#CB9AFE",
+					300: "#AA68FC",
+					400: "#8D42F9",
+					500: "#5F06F5",
+					600: "#4904D2",
+					700: "#3603B0",
+					800: "#26018E",
+					900: "#1A0175",
+				},
+				secondary: {
+					DEFAULT: "#D23B5D",
+					50: "#F8D0D6",
+					100: "#F0A8B3",
+					200: "#E77D91",
+					300: "#E05A6F",
+					400: "#D73A4D",
+					500: "#D23B5D",
+					600: "#B72F4F",
+					700: "#9B2440",
+					800: "#7F1932",
+					900: "#631127",
+				},
+				foreground: "#f0f0f4",
+			},
+		},
+	},
 });
