f4dae185bd51b25f4f336b0b5e00f292e56348f8
TheDanniCraft <mail@thedannicraft.de>
2025-08-31 15:43:58 +0200

    ✨ Add legal links to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@heroui/react";
+import { Button, Link } from "@heroui/react";
 import { IconBrandTwitch } from "@tabler/icons-react";
 import ErrorToast from "@components/errorToast";
-import Link from "next/link";
 
 export default async function Login({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
 	const scopes: string[] = ["user:read:email", "channel:read:redemptions", "channel:manage:redemptions", "user:write:chat", "user:bot", "channel:bot"];
@@ -25,6 +24,19 @@ export default async function Login({ searchParams }: { searchParams: Promise<{ 
 					<Button as={Link} href={authLink.toString()} startContent={<IconBrandTwitch color='#8956FB' />} variant='faded' size='lg' color='default' aria-label='Login with Twitch'>
 						Login with Twitch
 					</Button>
+					<div className='flex flex-col items-center text-xs text-gray-400 mt-1'>
+						<p>By using this service, you agree to our </p>
+						<p>
+							<Link isExternal href='https://hub.goadopt.io/document/9651af3f-af45-480f-8a4d-2beb6ed68e9b?language=en' className='text-xs' color='foreground'>
+								Terms of Service
+							</Link>{" "}
+							and{" "}
+							<Link isExternal href='https://hub.goadopt.io/document/3852d930-97b9-46c2-950d-823e62515ab4?language=en' className='text-xs' color='foreground'>
+								Privacy Policy
+							</Link>
+							.
+						</p>
+					</div>
 				</div>
 			</div>
 		</>
