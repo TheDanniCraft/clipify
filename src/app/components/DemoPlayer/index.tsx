d62d32541cbb4f9e3f10423319858ada65bd2a60
TheDanniCraft <mail@thedannicraft.de>
2025-12-22 14:58:05 +0100

    ✨ Add streaming software mock

---
+++ src/app/components/DemoPlayer/index.tsx
@@ -0,0 +1,9 @@
+import StreamingSoftwareMock from "./streamingSoftwareMock";
+
+export default function DemoPlayer() {
+	return (
+		<div className='w-full'>
+			<StreamingSoftwareMock>Awesome Overlay Demo</StreamingSoftwareMock>
+		</div>
+	);
+}
