faa83010a7a5353c23de2c5dfc14bc87113b6f71
TheDanniCraft <mail@thedannicraft.de>
2026-08-16 23:29:04 +0200

    🐛 Fix embed theme provider isolation

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { isEmbeddedRoute } from "@lib/embeddedRoutes";
+import { usePathname } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+	const pathname = usePathname();
+	if (isEmbeddedRoute(pathname)) return <>{children}</>;
+
 	return (
 		<NextThemesProvider attribute='class' defaultTheme='dark' enableSystem={false} disableTransitionOnChange>
 			{children}
